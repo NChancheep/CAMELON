85e41f13f3a0ea75f5f70ed0815d8f03bae09e33
Chancheep <chancheep.mcs@gmail.com>
2023-01-27 14:13:25 +0700

    Update dashboard.js

--- camelon-front/src/components/dashboard.js
+++ camelon-front/src/components/dashboard.js
@@ -1,8 +1,24 @@
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid'; // Grid version 1
+import Button from '@mui/material/Button';
+
+
 function dashboard() {
     return (
-      <div >
-        hello world xxx
-      </div>
+      <Container maxwidth="lg">
+        <h1>This is Container</h1>
+        <Grid container spacing={3}>
+          <Grid xs>
+          <Button variant="outlined">Overview</Button>
+          </Grid>
+          <Grid xs>
+          <Button variant="outlined">Map</Button>
+          </Grid>
+          <Grid xs>
+          <Button variant="outlined">Heatmap</Button>
+          </Grid>
+        </Grid>
+      </Container>
     );
   }
   
