ce96f24d96efe829a0fcca0d6378634eaa3b4eab
Chancheep <51604377+NChancheep@users.noreply.github.com>
2023-01-28 13:25:21 +0700

    dsd

--- camelon-front/src/components/dashboard.js
+++ camelon-front/src/components/dashboard.js
@@ -1,39 +1,47 @@
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import MySidebar from './cssFramework/sidebar/mySideBar';
+import Navbar from 'react-bootstrap/Navbar';
+// import MySidebar from './cssFramework/sidebar/mySideBar';
 function dashboard() {
     return (
-      
       <Container>
-        <MySidebar/>
-        <Row md={3}>
-          <Col>
-          <div className="mb-1">
-            <Button variant="primary" size="lg">
-              Large button
-            </Button>{' '}
-          </div>
-      </Col>
-      <Col>
-          <div className="mb-1">
-            <Button variant="primary" size="lg">
-              Large button
-            </Button>{' '}
-          </div>
-      </Col>
-      <Col>
-          <div className="mb-1">
-            <Button variant="primary" size="lg">
-              Large button
-            </Button>{' '}
-          </div>
-      </Col>
-        </Row>
+      <Navbar expand="lg" variant="light" bg="light">
+        <Container>
+          <Navbar.Brand href="#">Navbar</Navbar.Brand>
+        </Container>
+      </Navbar>
     </Container>
+      
     );
   }
   
   export default dashboard;
+
+
+  // <Container>
+        
+  //       <Row md={3}>
+  //         <Col>
+  //         <div className="mb-1">
+  //           <Button variant="primary" size="lg">
+  //             Large button
+  //           </Button>{' '}
+  //         </div>
+  //     </Col>
+  //     <Col>
+  //         <div className="mb-1">
+  //           <Button variant="primary" size="lg">
+  //             Large button
+  //           </Button>{' '}
+  //         </div>
+  //     </Col>
+  //     <Col>
+  //         <div className="mb-1">
+  //           <Button variant="primary" size="lg">
+  //             Large button
+  //           </Button>{' '}
+  //         </div>
+  //     </Col>
+  //       </Row>
+  //   </Container>
