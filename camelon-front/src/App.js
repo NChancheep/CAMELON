81254a46330a297e4a724843a117424c3b26910f
Chancheep <51604377+NChancheep@users.noreply.github.com>
2023-01-28 18:17:50 +0700

    update

--- camelon-front/src/App.js
+++ camelon-front/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from './components/dashboard';
+import Home from './components/home/home';
 import Aboutus from './components/aboutus';
 import CrimeMap from './components/crimeMap/crimeMap';
 function App() {
@@ -7,7 +7,7 @@ function App() {
   <BrowserRouter>
   <div>
     <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Home />} />
         <Route path="/aboutus" element={<Aboutus />} />
         <Route path="/crimemap" element={<CrimeMap />} />
       </Routes>
