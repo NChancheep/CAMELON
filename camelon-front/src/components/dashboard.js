fcfa3dccc254287e5c2b0ebc6717f0614532626c
Chancheep <51604377+NChancheep@users.noreply.github.com>
2023-01-28 15:52:00 +0700

    update

--- camelon-front/src/components/dashboard.js
+++ camelon-front/src/components/dashboard.js
@@ -1,16 +1,26 @@
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
-// import MySidebar from './cssFramework/sidebar/mySideBar';
+import MySidebar from './cssFramework/sidebar/mySideBar';
 function dashboard() {
     return (
-      <Container>
-      <Navbar expand="lg" variant="light" bg="light">
-        <Container>
-          <Navbar.Brand href="#">Navbar</Navbar.Brand>
-        </Container>
-      </Navbar>
+      <Container fluid>
+        <Row>
+        <Col>
+        <header>
+          Overview
+        </header>
+        </Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col><Button variant="outline-primary" href="#">Overview</Button></Col>
+        <Col><Button variant="outline-primary" href="/crimemap">Map</Button></Col>
+        <Col><Button variant="outline-primary" href="#">Heat Map</Button></Col>
+      </Row>
     </Container>
       
     );
