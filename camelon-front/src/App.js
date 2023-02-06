8b414b1bff7e71f9b0fd6911632571c45d500e97
Jump_km <kantapong101143@gmail.com>
2023-02-06 21:18:44 +0700

    ๊Update
    add Filter, edit map page, edit Heading, edit page align

--- camelon-front/src/App.js
+++ camelon-front/src/App.js
@@ -3,11 +3,10 @@ import React from "react";
 import "./css/Sidebar.css";
 import Sidebar2 from "./components/Sidebar2";
 
-// ...
 function App() {
   return (
     <main>
-      <div className="App" id="outer-container">
+      <div id="outer-container">
         <Sidebar2
           pageWrapId={"page-wrap"}
           outerContainerId={"outer-container"}
--- camelon-front/src/components/Modes/aboutus.js
+++ camelon-front/src/components/Modes/aboutus.js
@@ -1,65 +1,70 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 export default function Aboutus() {
-    
-    return (
-        <>
-        <div class="p-4 sm:ml-64">
-          <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+  return (
+    <>
+      <div class="sm" style={{marginTop:16}}>
+        <div class="border-2 border-gray-200 rounded">
           <Container>
-     
-      <Row>
-        <Col><Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="https://reactjs.org/logo-og.png" />
-            <Card.Body>
-              <Card.Title>Chancheep Mahacharoensuk</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card></Col>
+            <Row>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src="https://reactjs.org/logo-og.png"
+                  />
+                  <Card.Body>
+                    <Card.Title>Chancheep Mahacharoensuk</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
 
-        <Col><Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="https://reactjs.org/logo-og.png" />
-            <Card.Body>
-              <Card.Title>Pattadon Singhajan</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card></Col>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src="https://reactjs.org/logo-og.png"
+                  />
+                  <Card.Body>
+                    <Card.Title>Pattadon Singhajan</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
 
-        <Col> <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="https://reactjs.org/logo-og.png" />
-            <Card.Body>
-              <Card.Title>Kantapong Matangkarat</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card></Col>
-
-      </Row>
-    </Container>
-          
-          
-         
-          </div>
+              <Col>
+                {" "}
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src="https://reactjs.org/logo-og.png"
+                  />
+                  <Card.Body>
+                    <Card.Title>Kantapong Matangkarat</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
         </div>
-      </>
-    );
-          
-
-  }
-
-  
-
+      </div>
+    </>
+  );
+}
