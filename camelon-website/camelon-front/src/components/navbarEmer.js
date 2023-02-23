38024a377e7870a45377ff2b34937b958dc47e8c
Chancheep <chancheep.mcs@gmail.com>
2023-02-23 12:32:39 +0700

    update

---
+++ camelon-website/camelon-front/src/components/navbarEmer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Gambling from "./cards/Gambling";
+// import  from ;
+// import  from ;
+// import  from ;
+
+export default function NavbarEmer(props) {
+  if (props.data === "Gambling") {
+    return <Gambling/>;
+  } 
+  else if (props.data === "Murder") {
+    return <props />;
+  } 
+  else if (props.data === "SexualAbuse") {
+    return <props />;
+  } 
+  else if (props.data === "TheftBurglary") {
+    return < props/>;
+  }
+  else if (props.data === "BatteryAssault") {
+    return < props/>;
+  }
+  else if (props.data === "Drug") {
+    return <props />;
+  }
+  else if (props.data === "Accident") {
+    return < props/>;
+  }
+}
