d7917aed470b7703455f4846206d6ddc54250492
Jump_km <105689885+KantapongSmile@users.noreply.github.com>
2023-03-08 15:48:52 +0700

    Update

--- camelon-website/camelon-front/src/components/Filtter.js
+++ camelon-website/camelon-front/src/components/Filtter.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
-import Container from "react-bootstrap/Container";
+import React, { useState } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "../css/Filtter.css";
-import Axios from "axios";
+import Card from "react-bootstrap/Card";
 
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
@@ -19,24 +18,23 @@ function FiltterComponent() {
   const [districtList, setDistrictList] = useState([]);
   const [subdistrictList, setSubdistrictList] = useState([]);
   const [subdistrictId, setSubdistrictId] = useState();
-  
- 
+
   const getDistrictList = (province_id) => {
     const districts_list = districts.filter(
-      (district) => district.province_id == province_id
+      (district) => district.province_id === province_id
     );
     setDistrictList(districts_list);
   };
 
   const getSubdistrictList = (district_id) => {
     const subdistrict_list = subdistricts.filter(
-      (subdistrict) => subdistrict.district_id == district_id
+      (subdistrict) => subdistrict.district_id === district_id
     );
     setSubdistrictList(subdistrict_list);
   };
 
   const getLatLong = (id) => {
-    let position = subdistrictList.find((subdistrict) => subdistrict.id == id);
+    let position = subdistrictList.find((subdistrict) => subdistrict.id === id);
 
     const user_current_location = {
       latitude: position.latitude,
@@ -44,75 +42,82 @@ function FiltterComponent() {
     };
     dispatch(change_current_location(user_current_location));
   };
-  
 
   return (
-    <Container className={`nav-bar-select-locati`} style={{height:55}}>
-      <Row className="rectangle-1378">
-        <Col className="text">
-        <select
-          onChange={(e) => {
-            getDistrictList(e.target.value);
-          }}
-        >
-          <option value="" disabled selected>
-            จังหวัด
-          </option>
-          {provinces.map((val, key) => {
-            return <option value={val.id}>{val.name_in_thai}</option>;
-          })}
-        </select>
+    
+      <Row xs style={{ height: "100%",padding:10 }} className="justify-content-center align-items-center">
+        <Col sm>
+          <select
+            onChange={(e) => {
+              getDistrictList(e.target.value);
+            }}
+          >
+            <option value="" disabled selected>
+              จังหวัด
+            </option>
+            {provinces.map((val, key) => {
+              return <option value={val.id}>{val.name_in_thai}</option>;
+            })}
+          </select>
         </Col>
-        <Col className="text-1">
-        <select
-          onChange={(e) => {
-            getSubdistrictList(e.target.value);
-          }}
-        >
-          <option value="" disabled selected>
-            อำเภอ/เขต
-          </option>
-          {districtList.map((val, key) => {
-            return <option value={val.id}>{val.name_in_thai}</option>;
-          })}
-        </select>
+        <Col sm>
+          <select
+            onChange={(e) => {
+              getSubdistrictList(e.target.value);
+            }}
+          >
+            <option value="" disabled selected>
+              อำเภอ/เขต
+            </option>
+            {districtList.map((val, key) => {
+              return <option value={val.id}>{val.name_in_thai}</option>;
+            })}
+          </select>
         </Col>
-        <Col className="text-2">
-        <select
-          onChange={(e) => {
-            setSubdistrictId(e.target.value);
-          }}
-        >
-          <option value="" disabled selected>
-            เเขวง/ตำบล
-          </option>
-          {subdistrictList.map((val, key) => {
-            return <option value={val.id}>{val.name_in_thai}</option>;
-          })}
-        </select>
+        <Col sm>
+          <select
+            onChange={(e) => {
+              setSubdistrictId(e.target.value);
+            }}
+          >
+            <option value="" disabled selected>
+              เเขวง/ตำบล
+            </option>
+            {subdistrictList.map((val, key) => {
+              return <option value={val.id}>{val.name_in_thai}</option>;
+            })}
+          </select>
         </Col>
-        <Col className="text-3">
-        <select>
-              <option value="" disabled selected>ช่วงเวลา</option>
-              <option>safsa</option>
-        </select>
+        <Col sm >
+          <select>
+            <option value="" disabled selected>
+              ช่วงเวลา
+            </option>
+            <option>safsa</option>
+          </select>
         </Col>
-        <Col className="text-4">
-        <select>
-              <option value="" disabled selected>ประเภทอาชญากรรม</option>
-              <option>safsa</option>
-        </select>
+        <Col sm>
+          <select>
+            <option value="" disabled selected>
+              ประเภทอาชญากรรม
+            </option>
+            <option>safsa</option>
+          </select>
         </Col>
-        <Col className="rectangle-1379"> <button
-          type="submit"
-          onClick={() => {
-            getLatLong(subdistrictId);
-          }}
-        >
-          ค้นหา
-        </button></Col>
+        <Col>
+          {" "}
+          <button
+          style={{width:"100%"}}
+            class="bg-white text-black hover:bg-black hover:text-white font-bold border border-black rounded"
+            type="submit"
+            onClick={() => {
+              getLatLong(subdistrictId);
+            }}
+          >
+            ค้นหา
+          </button>
+        </Col>
       </Row>
-    </Container>
   );
 }
 
--- camelon-website/camelon-front/src/components/Modes/Gambling.js
+++ camelon-website/camelon-front/src/components/Modes/Gambling.js
@@ -1,6 +1,5 @@
 import React from "react";
 import EmergencyBanner from "../cards/emerContact";
-import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 const EmerContact = [
