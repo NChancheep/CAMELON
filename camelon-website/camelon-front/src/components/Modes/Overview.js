6d5f97a76ab002dc7169bdc952a9a56c6360095f
Chancheep <chancheep.mcs@gmail.com>
2023-03-09 16:03:29 +0700

    update bar chart

--- camelon-website/camelon-front/src/components/Modes/Overview.js
+++ camelon-website/camelon-front/src/components/Modes/Overview.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+import BarChart from "./BarChart";
 export default function Overview() {
   return (
     <div class="sm" style={{ marginTop: 16 }}>
       <div class="text-xl">Hey, this is Overview</div>
+      <BarChart />
     </div>
   );
 }
+
