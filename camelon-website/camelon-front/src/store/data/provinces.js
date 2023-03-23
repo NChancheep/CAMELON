9b74b4cf3a6fdb944f57d16efb91f62b53849dc2
Chancheep <chancheep.mcs@gmail.com>
2023-03-23 12:35:51 +0700

    Update provinces.js

--- camelon-website/camelon-front/src/store/data/provinces.js
+++ camelon-website/camelon-front/src/store/data/provinces.js
@@ -3,462 +3,617 @@ export const provinces = [
     "id": 13,
     "code": 10,
     "name_in_thai": "กรุงเทพมหานคร",
-    "name_in_english": "Bangkok"
+    "name_in_english": "Bangkok",
+    "latitude": 13.7278956,
+    "longitude": 100.52412349999997,
   },
   {
     "id": 18,
     "code": 11,
     "name_in_thai": "สมุทรปราการ",
-    "name_in_english": "Samut Prakarn"
+    "name_in_english": "Samut Prakarn",
+    "latitude": 13.5990961,
+    "longitude": 100.59983190000003,
+    
   },
   {
     "id": 23,
     "code": 12,
     "name_in_thai": "นนทบุรี",
-    "name_in_english": "Nonthaburi"
+    "name_in_english": "Nonthaburi",
+    "latitude": 13.8621125,
+    "longitude": 100.51435279999998,
   },
   {
     "id": 35,
     "code": 13,
     "name_in_thai": "ปทุมธานี",
-    "name_in_english": "Pathum Thani"
+    "name_in_english": "Pathum Thani",
+    "latitude": 14.0208391,
+    "longitude": 100.52502759999993,
   },
   {
     "id": 16,
     "code": 14,
     "name_in_thai": "พระนครศรีอยุธยา",
-    "name_in_english": "Phra Nakhon Si Ayutthaya"
+    "name_in_english": "Phra Nakhon Si Ayutthaya",
+    "latitude": 14.3532128,
+    "longitude": 100.56895989999998,
   },
   {
     "id": 19,
     "code": 15,
     "name_in_thai": "อ่างทอง",
-    "name_in_english": "Ang Thong"
+    "name_in_english": "Ang Thong",
+    "latitude": 14.5896054,
+    "longitude": 100.45505200000002,
   },
   {
     "id": 17,
     "code": 16,
     "name_in_thai": "ลพบุรี",
-    "name_in_english": "Lop Buri"
+    "name_in_english": "Lop Buri",
+    "latitude": 14.7995081,
+    "longitude": 100.65337060000002,
   },
   {
     "id": 28,
     "code": 17,
     "name_in_thai": "สิงห์บุรี",
-    "name_in_english": "Sing Buri"
+    "name_in_english": "Sing Buri",
+    "latitude": 14.8936253,
+    "longitude": 100.39673140000002,
   },
   {
     "id": 22,
     "code": 18,
     "name_in_thai": "ชัยนาท",
-    "name_in_english": "Chai Nat"
+    "name_in_english": "Chai Nat",
+    "latitude": 15.1851971,
+    "longitude": 100.12512500000003,
   },
   {
     "id": 32,
     "code": 19,
     "name_in_thai": "สระบุรี",
-    "name_in_english": "Saraburi"
+    "name_in_english": "Saraburi",
+    "latitude": 14.5289154,
+    "longitude": 100.91014210000003,
   },
   {
     "id": 5,
     "code": 20,
     "name_in_thai": "ชลบุรี",
-    "name_in_english": "Chon Buri"
+    "name_in_english": "Chon Buri",
+    "latitude": 13.3611431,
+    "longitude": 100.98467170000004,
   },
   {
     "id": 1,
     "code": 21,
     "name_in_thai": "ระยอง",
-    "name_in_english": "Rayong"
+    "name_in_english": "Rayong",
+    "latitude": 12.6833115,
+    "longitude": 101.23742949999996,
   },
   {
     "id": 7,
     "code": 22,
     "name_in_thai": "จันทบุรี",
-    "name_in_english": "Chanthaburi"
+    "name_in_english": "Chanthaburi",
+    "latitude": 12.61134,
+    "longitude": 102.10385459999998,
   },
   {
     "id": 9,
     "code": 23,
     "name_in_thai": "ตราด",
-    "name_in_english": "Trat"
+    "name_in_english": "Trat",
+    "latitude": 12.2427563,
+    "longitude": 102.51747339999997,
   },
   {
     "id": 37,
     "code": 24,
     "name_in_thai": "ฉะเชิงเทรา",
-    "name_in_english": "Chachoengsao"
+    "name_in_english": "Chachoengsao",
+    "latitude": 13.6904194,
+    "longitude": 101.07795959999999,
   },
   {
     "id": 55,
     "code": 25,
     "name_in_thai": "ปราจีนบุรี",
-    "name_in_english": "Prachin Buri"
+    "name_in_english": "Prachin Buri",
+    "latitude": 14.0509704,
+    "longitude": 101.37274389999993,
   },
   {
     "id": 31,
     "code": 26,
     "name_in_thai": "นครนายก",
-    "name_in_english": "Nakhon Nayok"
+    "name_in_english": "Nakhon Nayok",
+    "latitude": 14.2069466,
+    "longitude": 101.21305110000003,
   },
   {
     "id": 60,
     "code": 27,
     "name_in_thai": "สระแก้ว",
-    "name_in_english": "Sa kaeo"
+    "name_in_english": "Sa kaeo",
+    "latitude": 13.824038,
+    "longitude": 102.0645839,
   },
   {
     "id": 54,
     "code": 30,
     "name_in_thai": "นครราชสีมา",
-    "name_in_english": "Nakhon Ratchasima"
+    "name_in_english": "Nakhon Ratchasima",
+    "latitude": 14.9798997,
+    "longitude": 102.09776929999998,
   },
   {
     "id": 49,
     "code": 31,
     "name_in_thai": "บุรีรัมย์",
-    "name_in_english": "Buri Ram"
+    "name_in_english": "Buri Ram",
+    "latitude": 14.9930017,
+    "longitude": 103.10291910000001,
   },
   {
     "id": 69,
     "code": 32,
     "name_in_thai": "สุรินทร์",
-    "name_in_english": "Surin"
+    "name_in_english": "Surin",
+    "latitude": 14.882905,
+    "longitude": 103.49371070000007,
   },
   {
     "id": 63,
     "code": 33,
     "name_in_thai": "ศรีสะเกษ",
-    "name_in_english": "Si Sa Ket"
+    "name_in_english": "Si Sa Ket",
+    "latitude": 15.1186009,
+    "longitude": 104.32200949999992,
   },
   {
     "id": 42,
     "code": 34,
     "name_in_thai": "อุบลราชธานี",
-    "name_in_english": "Ubon Ratchathani"
+    "name_in_english": "Ubon Ratchathani",
+    "latitude": 15.2286861,
+    "longitude": 104.85642170000006,
   },
   {
     "id": 64,
     "code": 35,
     "name_in_thai": "ยโสธร",
-    "name_in_english": "Yasothon"
+    "name_in_english": "Yasothon",
+    "latitude": 15.792641,
+    "longitude": 104.14528270000005,
   },
   {
     "id": 71,
     "code": 36,
     "name_in_thai": "ชัยภูมิ",
-    "name_in_english": "Chaiyaphum"
+    "name_in_english": "Chaiyaphum",
+    "latitude": 15.8068173,
+    "longitude": 102.03150270000003,
   },
   {
     "id": 74,
     "code": 37,
     "name_in_thai": "อำนาจเจริญ",
-    "name_in_english": "Amnat Charoen"
+    "name_in_english": "Amnat Charoen",
+    "latitude": 15.8656783,
+    "longitude": 104.62577740000006,
   },
   {
     "id": 76,
     "code": 38,
     "name_in_thai": "บึงกาฬ",
-    "name_in_english": "Bueng Kan"
+    "name_in_english": "Bueng Kan",
+    "latitude": 18.3609104,
+    "longitude": 103.64644629999998,
   },
   {
     "id": 75,
     "code": 39,
     "name_in_thai": "หนองบัวลำภู",
-    "name_in_english": "Nong Bua Lam Phu"
+    "name_in_english": "Nong Bua Lam Phu",
+    "latitude": 17.2218247,
+    "longitude": 102.42603680000002,
   },
   {
     "id": 25,
     "code": 40,
     "name_in_thai": "ขอนแก่น",
-    "name_in_english": "Khon Kaen"
+    "name_in_english": "Khon Kaen",
+    "latitude": 16.4419355,
+    "longitude": 102.8359921,
   },
   {
     "id": 61,
     "code": 41,
     "name_in_thai": "อุดรธานี",
-    "name_in_english": "Udon Thani"
+    "name_in_english": "Udon Thani",
+    "latitude": 17.4138413,
+    "longitude": 102.78723250000007,
   },
   {
     "id": 67,
     "code": 42,
     "name_in_thai": "เลย",
-    "name_in_english": "Loei"
+    "name_in_english": "Loei",
+    "latitude": 17.4860232,
+    "longitude": 101.72230020000006,
   },
   {
     "id": 41,
     "code": 43,
     "name_in_thai": "หนองคาย",
-    "name_in_english": "Nong Khai"
+    "name_in_english": "Nong Khai",
+    "latitude": 17.8782803,
+    "longitude": 102.74126380000007,
   },
   {
     "id": 3,
     "code": 44,
     "name_in_thai": "มหาสารคาม",
-    "name_in_english": "Maha Sarakham"
+    "name_in_english": "Maha Sarakham",
+    "latitude": 16.1850896,
+    "longitude": 103.30264609999995,
   },
   {
     "id": 57,
     "code": 45,
     "name_in_thai": "ร้อยเอ็ด",
-    "name_in_english": "Roi Et"
+    "name_in_english": "Roi Et",
+    "latitude": 16.0538196,
+    "longitude": 103.65200359999994,
   },
   {
     "id": 68,
     "code": 46,
     "name_in_thai": "กาฬสินธุ์",
-    "name_in_english": "Kalasin"
+    "name_in_english": "Kalasin",
+    "latitude": 16.4314078,
+    "longitude": 103.5058755,
   },
   {
     "id": 72,
     "code": 47,
     "name_in_thai": "สกลนคร",
-    "name_in_english": "Sakon Nakhon"
+    "name_in_english": "Sakon Nakhon",
+    "latitude": 17.1545995,
+    "longitude": 104.1348365,
   },
   {
     "id": 58,
     "code": 48,
     "name_in_thai": "นครพนม",
-    "name_in_english": "Nakhon Phanom"
+    "name_in_english": "Nakhon Phanom",
+    "latitude": 17.392039,
+    "longitude": 104.76955079999993,
   },
   {
     "id": 59,
     "code": 49,
     "name_in_thai": "มุกดาหาร",
-    "name_in_english": "Mukdahan"
+    "name_in_english": "Mukdahan",
+    "latitude": 16.542443,
+    "longitude": 104.72091509999996,
   },
   {
     "id": 21,
     "code": 50,
     "name_in_thai": "เชียงใหม่",
-    "name_in_english": "Chiang Mai"
+    "name_in_english": "Chiang Mai",
+    "latitude": 18.7877477,
+    "longitude": 98.99313110000003,
   },
   {
     "id": 40,
     "code": 51,
     "name_in_thai": "ลำพูน",
-    "name_in_english": "Lamphun"
+    "name_in_english": "Lamphun",
+    "latitude": 18.5744606,
+    "longitude": 99.0087221,
   },
   {
     "id": 62,
     "code": 52,
     "name_in_thai": "ลำปาง",
-    "name_in_english": "Lampang"
+    "name_in_english": "Lampang",
+    "latitude": 18.2888404,
+    "longitude": 99.49087399999996,
   },
   {
     "id": 24,
     "code": 53,
     "name_in_thai": "อุตรดิตถ์",
-    "name_in_english": "Uttaradit"
+    "name_in_english": "Uttaradit",
+    "latitude": 17.6200886,
+    "longitude": 100.09929420000003,
   },
   {
     "id": 46,
     "code": 54,
     "name_in_thai": "แพร่",
-    "name_in_english": "Phrae"
+    "name_in_english": "Phrae",
+    "latitude": 18.1445774,
+    "longitude": 100.14028310000003,
   },
   {
     "id": 51,
     "code": 55,
     "name_in_thai": "น่าน",
-    "name_in_english": "Nan"
+    "name_in_english": "Nan",
+    "latitude": 18.7756318,
+    "longitude": 100.77304170000002,
   },
   {
     "id": 44,
     "code": 56,
     "name_in_thai": "พะเยา",
-    "name_in_english": "Phayao"
+    "name_in_english": "Phayao",
+    "latitude": 19.1664789,
+    "longitude": 99.9019419,
   },
   {
     "id": 38,
     "code": 57,
     "name_in_thai": "เชียงราย",
-    "name_in_english": "Chiang Rai"
+    "name_in_english": "Chiang Rai",
+    "latitude": 19.9071656,
+    "longitude": 99.83095500000002,
   },
   {
     "id": 48,
     "code": 58,
     "name_in_thai": "แม่ฮ่องสอน",
-    "name_in_english": "Mae Hong Son"
+    "name_in_english": "Mae Hong Son",
+    "latitude": 19.2990643,
+    "longitude": 97.96562259999996,
   },
   {
     "id": 26,
     "code": 60,
     "name_in_thai": "นครสวรรค์",
-    "name_in_english": "Nakhon Sawan"
+    "name_in_english": "Nakhon Sawan",
+    "latitude": 15.6930072,
+    "longitude": 100.12255949999997,
   },
   {
     "id": 45,
     "code": 61,
     "name_in_thai": "อุทัยธานี",
-    "name_in_english": "Uthai Thani"
+    "name_in_english": "Uthai Thani",
+    "latitude": 15.3835001,
+    "longitude": 100.02455269999996,
   },
   {
     "id": 73,
     "code": 62,
     "name_in_thai": "กำแพงเพชร",
-    "name_in_english": "Kamphaeng Phet"
+    "name_in_english": "Kamphaeng Phet",
+    "latitude": 16.4827798,
+    "longitude": 99.52266179999992,
   },
   {
     "id": 56,
     "code": 63,
     "name_in_thai": "ตาก",
-    "name_in_english": "Tak"
+    "name_in_english": "Tak",
+    "latitude": 16.8839901,
+    "longitude": 99.12584979999997,
   },
   {
     "id": 43,
     "code": 64,
     "name_in_thai": "สุโขทัย",
-    "name_in_english": "Sukhothai"
+    "name_in_english": "Sukhothai",
+    "latitude": 17.0055573,
+    "longitude": 99.82637120000004,
   },
   {
     "id": 36,
     "code": 65,
     "name_in_thai": "พิษณุโลก",
-    "name_in_english": "Phitsanulok"
+    "name_in_english": "Phitsanulok",
+    "latitude": 16.8298048,
+    "longitude": 100.26149150000003,
   },
   {
     "id": 50,
     "code": 66,
     "name_in_thai": "พิจิตร",
-    "name_in_english": "Phichit"
+    "name_in_english": "Phichit",
+    "latitude": 16.4429516,
+    "longitude": 100.34823289999997,
   },
   {
     "id": 47,
     "code": 67,
     "name_in_thai": "เพชรบูรณ์",
-    "name_in_english": "Phetchabun"
+    "name_in_english": "Phetchabun",
+    "latitude": 16.4189807,
+    "longitude": 101.15509259999999,
   },
   {
     "id": 30,
     "code": 70,
     "name_in_thai": "ราชบุรี",
-    "name_in_english": "Ratchaburi"
+    "name_in_english": "Ratchaburi",
+    "latitude": 13.5282893,
+    "longitude": 99.81342110000003,
   },
   {
     "id": 34,
     "code": 71,
     "name_in_thai": "กาญจนบุรี",
-    "name_in_english": "Kanchanaburi"
+    "name_in_english": "Kanchanaburi",
+    "latitude": 14.0227797,
+    "longitude": 99.53281149999998,
   },
   {
     "id": 53,
     "code": 72,
     "name_in_thai": "สุพรรณบุรี",
-    "name_in_english": "Suphan Buri"
+    "name_in_english": "Suphan Buri",
+    "latitude": 14.4744892,
+    "longitude": 100.11771279999994,
   },
   {
     "id": 33,
     "code": 73,
     "name_in_thai": "นครปฐม",
-    "name_in_english": "Nakhon Pathom"
+    "name_in_english": "Nakhon Pathom",
+    "latitude": 13.8199206,
+    "longitude": 100.06216760000007,
   },
   {
     "id": 15,
     "code": 74,
     "name_in_thai": "สมุทรสาคร",
-    "name_in_english": "Samut Sakhon"
+    "name_in_english": "Samut Sakhon",
+    "latitude": 13.5475216,
+    "longitude": 100.27439559999993,
   },
   {
     "id": 29,
     "code": 75,
     "name_in_thai": "สมุทรสงคราม",
-    "name_in_english": "Samut Songkhram"
+    "name_in_english": "Samut Songkhram",
+    "latitude": 13.4098217,
+    "longitude": 100.00226450000002,
   },
   {
     "id": 27,
     "code": 76,
     "name_in_thai": "เพชรบุรี",
-    "name_in_english": "Phetchaburi"
+    "name_in_english": "Phetchaburi",
+    "latitude": 13.1111601,
+    "longitude": 99.93913069999996,
   },
   {
     "id": 70,
     "code": 77,
     "name_in_thai": "ประจวบคีรีขันธ์",
-    "name_in_english": "Prachuap Khiri Khan"
+    "name_in_english": "Prachuap Khiri Khan",
+    "latitude": 11.812367,
+    "longitude": 99.79732709999996,
   },
   {
     "id": 14,
     "code": 80,
     "name_in_thai": "นครศรีธรรมราช",
-    "name_in_english": "Nakhon Si Thammarat"
+    "name_in_english": "Nakhon Si Thammarat",
+    "latitude": 8.4303975,
+    "longitude": 99.96312190000003,
   },
   {
     "id": 12,
     "code": 81,
     "name_in_thai": "กระบี่",
-    "name_in_english": "Krabi"
+    "name_in_english": "Krabi",
+    "latitude": 8.0862997,
+    "longitude": 98.90628349999997,
   },
   {
     "id": 8,
     "code": 82,
     "name_in_thai": "พังงา",
-    "name_in_english": "Phang-nga"
+    "name_in_english": "Phang-nga",
+    "latitude": 8.4407456,
+    "longitude": 98.51930319999997,
   },
   {
     "id": 11,
     "code": 83,
     "name_in_thai": "ภูเก็ต",
-    "name_in_english": "Phuket"
+    "name_in_english": "Phuket",
+    "latitude": 7.9810496,
+    "longitude": 98.36388239999997,
   },
   {
     "id": 4,
     "code": 84,
     "name_in_thai": "สุราษฎร์ธานี",
-    "name_in_english": "Surat Thani"
+    "name_in_english": "Surat Thani",
+    "latitude": 9.1382389,
+    "longitude": 99.32174829999997,
   },
   {
     "id": 10,
     "code": 85,
     "name_in_thai": "ระนอง",
-    "name_in_english": "Ranong"
+    "name_in_english": "Ranong",
+    "latitude": 9.9528702,
+    "longitude":98.60846409999999 ,
   },
   {
     "id": 2,
     "code": 86,
     "name_in_thai": "ชุมพร",
-    "name_in_english": "Chumphon"
+    "name_in_english": "Chumphon",
+    "latitude": 10.4930496,
+    "longitude": 99.18001989999993,
   },
   {
     "id": 39,
     "code": 90,
     "name_in_thai": "สงขลา",
-    "name_in_english": "Songkhla"
+    "name_in_english": "Songkhla",
+    "latitude": 7.1756004,
+    "longitude": 100.61434699999995,
   },
   {
     "id": 0,
     "code": 91,
     "name_in_thai": "สตูล",
-    "name_in_english": "Satun"
+    "name_in_english": "Satun",
+    "latitude": 6.6238158,
+    "longitude": 100.06737440000006,
   },
   {
     "id": 6,
     "code": 92,
     "name_in_thai": "ตรัง",
-    "name_in_english": "Trang"
+    "name_in_english": "Trang",
+    "latitude": 7.5593851,
+    "longitude": 99.61100650000003,
   },
   {
     "id": 65,
     "code": 93,
     "name_in_thai": "พัทลุง",
-    "name_in_english": "Phatthalung"
+    "name_in_english": "Phatthalung",
+    "latitude": 7.6166823,
+    "longitude": 100.07402309999998,
   },
   {
     "id": 20,
     "code": 94,
     "name_in_thai": "ปัตตานี",
-    "name_in_english": "Pattani"
+    "name_in_english": "Pattani",
+    "latitude": 6.869484399999999,
+    "longitude": 101.25048259999994,
   },
   {
     "id": 52,
     "code": 95,
     "name_in_thai": "ยะลา",
-    "name_in_english": "Yala"
+    "name_in_english": "Yala",
+    "latitude": 6.541147,
+    "longitude": 101.28039469999999,
   },
   {
     "id": 66,
     "code": 96,
     "name_in_thai": "นราธิวาส",
-    "name_in_english": "Narathiwat"
+    "name_in_english": "Narathiwat",
+    "latitude": 6.4254607,
+    "longitude": 101.82531429999995,
   }
 ]
