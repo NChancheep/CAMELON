26cb58320d988fda05366c8e7de2ec39a30ac377
Jump_km <105689885+KantapongSmile@users.noreply.github.com>
2023-03-30 17:13:46 +0700

    up

---
+++ camelon-website/camelon-front/src/components/ColorType.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "../css/Filtter.css";
+
+function ColorType() {
+  return (
+    <Row
+      xs
+      style={{ height: "100%", padding: 10, fontFamily: "Kanit" }}
+      className="justify-content-center align-items-center"
+    >
+      <Col sm>
+        <div className="legend">
+          <div className="legend" style={{ "--color": "#9400D3" }}>
+            การพนัน
+          </div>
+        </div>
+      </Col>
+      <Col sm>
+        <div className="legend">
+          <div className="legend" style={{ "--color": "#FF0000" }}>
+            ฆาตกรรม
+          </div>
+        </div>
+      </Col>
+      <Col sm>
+        <div className="legend">
+          <div className="legend" style={{ "--color": "#4B0082" }}>
+            ล่วงละเมิดทางเพศ
+          </div>
+        </div>
+      </Col>
+      <Col sm>
+        <div className="legend">
+          <div className="legend" style={{ "--color": "#FFFF00" }}>
+            ลักทรัพย์
+          </div>
+        </div>
+      </Col>
+      <Col sm>
+        <div className="legend">
+          <div className="legend" style={{ "--color": "#44985B" }}>
+            ทำร้ายร่างกาย
+          </div>
+        </div>
+      </Col>
+      <Col sm>
+        <div className="legend">
+          <div className="legend" style={{ "--color": "#FF7F00" }}>
+            ยาเสพติด
+          </div>
+        </div>
+      </Col>
+      <Col sm>
+        <div className="legend">
+          <div className="legend" style={{ "--color": "#0000FF" }}>
+            อุบัติเหตุ
+          </div>
+        </div>
+      </Col>
+    </Row>
+  );
+}
+
+export default ColorType;
--- camelon-website/camelon-front/src/components/controller/MenuController.js
+++ camelon-website/camelon-front/src/components/controller/MenuController.js
@@ -4,6 +4,7 @@ import Display from "../Display";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Filtter from "../Filtter";
+import ColorType from "../ColorType";
 import Heading from "../Heading";
 import Card from "react-bootstrap/Card";
 export default function MenuController() {
@@ -135,12 +136,17 @@ export default function MenuController() {
 
         <Row xs>
           <Col>
-            {state.mode === "overview" || state.mode === "choropleth_map"? (
+            {state.mode === "overview" || state.mode === "choropleth_map" ? (
               <div></div>
             ) : (
-              <Card>
-                <Filtter></Filtter>
-              </Card>
+              <div>
+                <Card>
+                  <Filtter></Filtter>
+                </Card>
+                <Card style={{marginTop:"1%"}}>
+                  <ColorType></ColorType>
+                </Card>
+              </div>
             )}
           </Col>
         </Row>
